89118648dc5a45ba5f8b669f4dd8ae800265f59c
Anishka Chauhan <anishkachauhan@Anishkas-MacBook-Pro.local>
2024-11-18 14:02:26 -0800

    created MancalaTester

--- Mancala/src/mancala/MancalaTester.java
+++ Mancala/src/mancala/MancalaTester.java
@@ -1,5 +1,19 @@
 package mancala;
+import java.awt.Frame;
+
+import javax.swing.*; 
 
 public class MancalaTester {
-
+	public static void main(String[] args) {
+		JFrame gameframe = new JFrame(); 
+		gameframe.setTitle("Mancala Game");
+		gameframe.setSize(800, 500);
+		
+		gameframe.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+		
+		
+		
+		//gameframe.pack(); 
+		gameframe.setVisible(true);
+	}
 }
