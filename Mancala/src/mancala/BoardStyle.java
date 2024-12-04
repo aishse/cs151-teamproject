36526b88fa2d0a471a4488d6311b6576610848b6
Jaspreet <jsaujla17@gmail.com>
2024-12-03 18:03:49 -0800

    writing javadoc comments

--- Mancala/src/mancala/BoardStyle.java
+++ Mancala/src/mancala/BoardStyle.java
@@ -2,6 +2,12 @@ package mancala;
 import java.awt.*;
 import java.awt.geom.*;
 
+/**
+ * This is the code for BoardStyle interface that is used to define different styles for the Mancala board.
+ * It has fixed dimensions and position for the board and methods to
+ * get the style's name and draw the board.
+ */
+
 public interface BoardStyle {
 	public final int BOARD_WIDTH = 650;
 	public final int BOARD_HEIGHT = 300;
