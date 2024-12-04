d1a5001929dfcbd29582163930dec88bc2ca1bb0
Jaspreet <jsaujla17@gmail.com>
2024-12-03 22:17:59 -0800

    finished comments

--- Mancala/src/mancala/StonePitListener.java
+++ Mancala/src/mancala/StonePitListener.java
@@ -1,6 +1,16 @@
 package mancala;
 
+/**
+ * The StonePitListener interface defines a listener for handling events
+ * when a StonePit is clicked. Classes that implement this interface can
+ * respond to clicks on a StonePit.
+ */
 public interface StonePitListener {
 
+	/**
+	 * This method is called when a StonePit is clicked.
+	 *
+	 * @param pit The StonePit that was clicked.
+	 */
 	void clicked(StonePit pit); 
 }
